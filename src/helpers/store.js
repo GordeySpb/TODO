168d38a4ac142e72faaccb5656c0052293ddcc5b
Kirill Gordeew <gordey_89@mail.ru>
2018-07-08 15:25:06 +0300

    store

---
+++ src/helpers/store.js
@@ -0,0 +1,31 @@
+import {createStore, dispatch} from 'redux';
+import {ADD_TODOS, ADD_TODO, DELETE, UPDATE} from './actions';
+
+const initialState = [];
+
+function reducer(state = initialState, {type, payload}) {
+    switch (type) {
+        case ADD_TODOS:
+            return payload;
+        case ADD_TODO:
+            return  [...state, payload];
+        case DELETE:
+            return state.filter(elem => elem.id !== +payload.id);
+        case UPDATE:
+            return state.map(elem => {
+                if (elem.id === +payload.id) {
+                    return {
+                        payload 
+                    }
+                }
+                return elem;
+            })
+        default:
+           return state; 
+    }
+}
+
+
+const store = createStore(reducer, initialState);
+
+module.exports = store;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,52 @@
 import './scss/main.scss';
 import tempalte from './templates/todo.hbs';
 import pubSub from './helpers/pubsub.js';
-
+import store from './helpers/store';
+import {addTodosAction, addTodoAction, deleteAction, updateAction} from './helpers/actions';
+import {ADD_TODOS, ADD_TODO, DELETE, UPDATE} from './helpers/actions';
 
 
 
 const todo = document.querySelector('.todo__list');
 const addBtn = document.querySelector('.todo__add-btn');
-const saveBtn = document.querySelector('.todo__save-btn');
 const input = document.querySelector('.todo-input');
 
 
-let store = [];
-let inputValue = '';
 
 
-function addToStore(item) {
-    store.push(item);
-
-    pubSub.emit('update')
-}
-
-function render() {
+function render(store) {
     const html = tempalte({
         items: store
     })
 
     todo.innerHTML = html;
-}
+};
 
 
-input.addEventListener('keyup', e => {
-    inputValue = e.target.value;
+addBtn.addEventListener('click', e => {
+    if (input.value === '') return;
 
-})
+    store.dispatch(addTodoAction({
+        name: input.value,
+        id: Date.now()
+    }))
+
+    input.value = '';
+    
+});
+
+
 
 todo.addEventListener('click', e => {
-    const li = document.querySelector('.todo__item');
-    const id = li.getAttribute('data-id');
     const allLi = document.querySelectorAll('.todo__item');
 
 
     if (e.target.classList.contains('todo__delete-btn')) {
-        pubSub.emit('delTask', id)
+        const deleteBtnId = +e.target.getAttribute('data-id');
+
+        store.dispatch(deleteAction({
+            id: deleteBtnId
+        }))
 
     }
 
@@ -50,9 +54,8 @@ todo.addEventListener('click', e => {
     if (e.target.classList.contains('js-edit-btn')) {
         const editBtnId = +e.target.getAttribute('data-id');
         const currentLi = findCurrentIdElement(allLi, editBtnId);
-        const currentStateElement = store.find(todo => todo.id === editBtnId);
+        const currentStateElement = store.getState().find(todo => todo.id === editBtnId);
         const input = currentLi.querySelector('.todo__input');
-
         input.value = currentStateElement.name;
         currentLi.classList.add('todo__item_mode_edit');
     }
@@ -61,12 +64,23 @@ todo.addEventListener('click', e => {
     if (e.target.classList.contains('js-save-btn')) {
         const saveBtnId = +e.target.getAttribute('data-id');
         const currentLi = findCurrentIdElement(allLi, saveBtnId);
+        const id = +currentLi.getAttribute('data-id');
+        const input = currentLi.querySelector('.todo__input');
+        const newValue = input.value;
+
+        store.dispatch(updateAction({
+            name: newValue,
+            id: id
+        }))
+
 
         currentLi.classList.remove('todo__item_mode_edit');
-        
+
+
     }
 
 })
+
 
 function findCurrentIdElement(allLi, searchId) {
     return Array.prototype.find.call(allLi, (elem) => {
@@ -74,31 +88,55 @@ function findCurrentIdElement(allLi, searchId) {
     })
 }
 
+store.subscribe(() => {
+    render(store.getState());
+});
+
+
+// function saveToStore(data) {
+//     const {
+//         id,
+//         newValue
+//     } = data;
+
+//     store = store.map(elem => {
+//         if (elem.id === id) {
+//             elem.name = newValue;
+//             elem.id = id
+//         }
+
+//         return elem;
+//     })
+
+//     pubSub.emit('updated', {
+//         store
+//     });
+// }
 
 
 
 
-function deleteTask(id) {
-    store = store.filter(elem => elem.id !== +id)
 
-    pubSub.emit('update')
-}
+// function saveToLocalStorage() {
+//     const list = JSON.stringify(store);
+//     localStorage.setItem('list', list)
 
+// };
 
-addBtn.addEventListener('click', e => {
-    if (inputValue === '') return;
-
-    pubSub.emit('addTodo', {
-        name: inputValue,
-        id: Date.now()
-    })
-
-    input.value = '';
-
-})
+// function updated(store) {
+//     render(store);
+//     saveToLocalStorage(store);
+// }
 
 
-pubSub.subscribe('addTodo', addToStore);
-pubSub.subscribe('delTask', deleteTask);
-// pubSub.subscribe('editTask', editTask)
-pubSub.subscribe('update', render);
+
+
+
+
+// pubSub.subscribe('onLoad', onLoadHandler);
+// pubSub.subscribe('editTodo', saveToStore);
+// pubSub.subscribe('addTodo', addToStore);
+// pubSub.subscribe('delTask', deleteTask);
+// pubSub.subscribe('saveToStorage', saveToLocalStorage);
+// pubSub.subscribe('updated', updated);
+// pubSub.emit('onLoad');
